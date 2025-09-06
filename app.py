1ad5e702583104bb9b49cad77dd53803c421c6b0
Naved20 <naved2010m.com>
2025-09-06 20:16:26 +0530

    after clear this

--- app.py
+++ app.py
@@ -37,13 +37,7 @@ def allowed_file(filename):
 
 #---------------DATABASE CONFIGURATION----------------
     
-uri = os.getenv("DATABASE_URL")
-
-# Convert postgres:// → postgresql://
-if uri and uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = uri or "sqlite:///mentors_connect.db"
+app.config["SQLALCHEMY_DATABASE_URI"] ="sqlite:///mentors_connect.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
