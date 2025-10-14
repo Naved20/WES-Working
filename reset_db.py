7069f52e83d647051adcfd4f7a4d5304e196b081
Naved20 <naved2010m.com>
2025-10-14 23:55:01 +0530

    final live

--- reset_db.py
+++ reset_db.py
@@ -1,4 +1,5 @@
 from app import db, app  # import both db and Flask app
+from app import User, MentorProfile, MenteeProfile, SupervisorProfile, MentorshipRequest, MeetingRequest
 
 with app.app_context():   # set app context
     db.drop_all()         # drop all tables
