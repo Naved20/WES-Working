893a57e27d549120f8f0df696d70401d90ea760f
Naved20 <naved2010m@gmail.com>
2025-11-06 18:56:43 +0530

    make structure of templete

--- reset_db.py
+++ reset_db.py
@@ -1,8 +1,8 @@
-from app import db, app  # import both db and Flask app
-from app import User, MentorProfile, MenteeProfile, SupervisorProfile, MentorshipRequest, MeetingRequest
+# from app import db, app  # import both db and Flask app
+# from app import User, MentorProfile, MenteeProfile, SupervisorProfile, MentorshipRequest, MeetingRequest
 
-with app.app_context():   # set app context
-    db.drop_all()         # drop all tables
-    db.create_all()       # recreate tables
+# with app.app_context():   # set app context
+#     db.drop_all()         # drop all tables
+#     db.create_all()       # recreate tables
 
-print("Database reset complete!")
+# print("Database reset complete!")
