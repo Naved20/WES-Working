bf4e66db9ba54aae67bff3589a5e17d02be2b82d
Naved Mansoori <naved2010m@gmail.com>
2025-11-22 11:54:53 +0530

    chace not working so we go back

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,38 +1,14 @@
-const CACHE_NAME = "wes-pwa-v2";
-
-const urlsToCache = [
-  "/",
-  "/signin",
-  "/signup",
-
-  "/supervisordashboard",
-  "/mentordashboard",
-  "/menteedashboard",
-
-  "/profile",
-
-  "/supervisor_tasks",
-  "/mentor_tasks",
-  "/mentee_tasks",
-
-  "/supervisor_calendar",
-  "/mentor_calendar",
-  "/mentee_calendar",
-
-  "/static/img/logo.jpg",
-  "/static/manifest.json",
-  "/static/service-worker.js",
-];
-
-// Install SW
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        '/',
+      ]);
+    })
   );
 });
 
-// Fetch (offline first)
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
