c9b03234e3866693d777a8a6c8fb108850cbdb34
Naved Mansoori <naved2010m@gmail.com>
2025-11-22 11:40:50 +0530

    add chace for save data in local for pwa 2nd time after chnes some things

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,28 +1,40 @@
-const CACHE_NAME = "flask-pwa-v1";
+const CACHE_NAME = "wes-pwa-v2";
 
 const urlsToCache = [
-  "/",             // home
-  "/login",
+  "/",
+  "/signin",
   "/signup",
-  "/dashboard",
+
+  "/supervisordashboard",
+  "/mentordashboard",
+  "/menteedashboard",
+
   "/profile",
-  "/static/css/style.css",
-  "/static/js/app.js",
-  "/static/icons/icon-192.png",
-  "/static/icons/icon-512.png"
+
+  "/supervisor_tasks",
+  "/mentor_tasks",
+  "/mentee_tasks",
+
+  "/supervisor_calendar",
+  "/mentor_calendar",
+  "/mentee_calendar",
+
+  "/static/img/logo.jpg",
+  "/static/manifest.json",
+  "/static/service-worker.js",
 ];
 
-// Install
-self.addEventListener("install", (event) => {
+// Install SW
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch
-self.addEventListener("fetch", (event) => {
+// Fetch (offline first)
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
