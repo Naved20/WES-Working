0c2c34e38a6b0661d88b84c4827284ed6abd7d4f
Naved Mansoori <naved2010m@gmail.com>
2025-11-22 11:21:07 +0530

    add chace for save data in local for pwa

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,16 +1,28 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = "flask-pwa-v1";
+
+const urlsToCache = [
+  "/",             // home
+  "/login",
+  "/signup",
+  "/dashboard",
+  "/profile",
+  "/static/css/style.css",
+  "/static/js/app.js",
+  "/static/icons/icon-192.png",
+  "/static/icons/icon-512.png"
+];
+
+// Install
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('v1').then(cache => {
-      return cache.addAll([
-        '/',
-      ]);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
+// Fetch
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
