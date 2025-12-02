f2d7f8691eb31fe8bde39d1d313a3901249cfac6
Naved Mansoori <naved2010m@gmail.com>
2025-12-03 01:47:50 +0530

    add supervisor can see institutes

--- zextra/reset_db.py
+++ zextra/reset_db.py
@@ -1,8 +1,11 @@
+# reset_db.py
 from app import db, app  # import both db and Flask app
 from app import MasterTask
 
 with app.app_context():   # set app context
-    db.drop_all()         # drop all tables
-    db.create_all()       # recreate tables
+    db.session.query(MasterTask).delete()
+    db.session.commit()  
+    
+    print("MasterTask table records deleted!")
 
 print("Database reset complete!")
